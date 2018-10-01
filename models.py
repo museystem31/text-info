a92efa2378ee1a36c45b5f76a60cb27a50fc105f
museystem31 <museystem31@gmail.com>
2018-10-01 11:25:41 -0700

    implemented models: Document, Page, Text, FontSize, and FontSizeStats

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import json
 import codecs
+import operator
+from collections import Counter
 '''
 Document stats: word count, line count, font sizes stats, pages stats
 #Page stats: word count, line count, lines stats, font sizes stats
@@ -13,6 +15,13 @@ class Document:
     def __init__(self, pdf):
         # create pages
         self.pages = self.createPages(pdf)
+        # create font size stats
+        self.docWordCount = self.getDocWordCount(self.pages)
+        self.docLineCount = self.getDocLineCount(self.pages)
+        docTexts = []
+        for page in self.pages:
+            docTexts += page["pageTexts"]
+        self.docFontSizeStats = vars(FontSizeStats(docTexts, self.docWordCount))
 
     def createPages(self, pdf):
         pages = []
@@ -23,15 +32,29 @@ class Document:
             pages.append(page)
         
         return pages
+    
+    def getDocWordCount(self, pages):
+        docWordCount = 0
+        for page in pages:
+            docWordCount += page["pageWordCount"]
+        return docWordCount
+    
+    def getDocLineCount(self, pages):
+        docLineCount = 0
+        for page in pages:
+            docLineCount += page["pageLineCount"]
+        return docLineCount
 
 class Page:
 
     def __init__(self, textItems, pageNumber):
         self.pageNumber = pageNumber
-        self.texts = self.createTexts(textItems)
-        #print(pageNumber)
+        self.pageTexts = self.createPageTexts(textItems)
+        self.pageWordCount = self.getPageWordCount(self.pageTexts)
+        self.pageLineCount = self.getPageLineCount(self.pageTexts)
+        self.pageFontSizeStats = vars(FontSizeStats(self.pageTexts, self.pageWordCount))
 
-    def createTexts(self, textItems):
+    def createPageTexts(self, textItems):
         texts = []
         
         for item in textItems:
@@ -40,6 +63,15 @@ class Page:
                 texts.append(text)
         
         return texts
+    
+    def getPageWordCount(self, texts):
+        wordCount = 0
+        for text in texts:
+            wordCount += text["wordCount"]
+        return wordCount
+
+    def getPageLineCount(self, pageTexts):
+        return len(pageTexts)
 
 class Text:
 
@@ -52,15 +84,40 @@ class Text:
         words = str.split()
         return len(words)
 
-"""
-class Font:
-    def __init__(self, family, size, count, frequency, deviationFromMode):
-        self.family = family
+
+class FontSize:
+    def __init__(self, size, count, mode, totalCount):
         self.size = size
         self.count = count
-        self.frequency = frequency
-        self.deviationFromMode = deviationFromMode
-"""
+        self.frequency = self.getFrequency(count, totalCount)
+        self.deviationFromMode = self.getDeviationFromMode(mode)
+    
+    def getFrequency(self, count, totalCount):
+        return float(count)/totalCount
+    
+    def getDeviationFromMode(self, mode):
+        return self.size - mode
+
+class FontSizeStats:
+    def __init__(self, texts, totalWordCount):
+        # get all font sizes
+        fontSizes = {}
+        for item in texts:
+            size = item["fontSize"]
+            if size in fontSizes:
+                fontSizes[size] += item["wordCount"]
+            else:
+                fontSizes[size] = item["wordCount"]
+        
+        self.fontSizes = fontSizes
+
+        # get mode, largest, and smallest font size
+        self.modeFontSize = max(fontSizes, key=lambda key: fontSizes[key])
+        self.maxFontSize = max(k for k, v in fontSizes.iteritems())
+        self.minFontSize = min(k for k, v in fontSizes.iteritems())
+
+        for fontSize, count in self.fontSizes.items():
+            self.fontSizes[fontSize] = vars(FontSize(fontSize, count, self.modeFontSize, totalWordCount))
 
 
 
