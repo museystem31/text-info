70392182fbd61d9154a258328f3218d6297362b4
muse83831 <muse83831@hotmail.com>
2018-10-04 23:14:08 -0700

    added ratio feature

--- application.py
+++ application.py
@@ -5,8 +5,52 @@ from flask import Flask, jsonify, render_template, request
 from models import Document, Page, Text
 import json
 import sys
+import time
+from scipy import stats
 
 application = Flask(__name__, static_folder='static')
+
+def normalize(val, min, max):
+    return (val-min)/(max-min)
+
+def processTexts(pdf):
+    # create a list of font sizes
+    # create a list of spacings
+    for page in pdf:
+        fontSizes = []
+        spacings = []
+        for text in page:
+            fontSizes.append(text["fontSize"])
+            if text["spacing"] > 0:
+                spacings.append(text["spacing"])
+        fontSizes.sort()
+        spacings.sort()
+        
+        # get max and min for font sizes and spacings
+        maxFontSize = fontSizes[len(fontSizes)-1]
+        minFontSize = fontSizes[0]
+
+        maxSpacing = spacings[len(spacings)-1]
+        minSpacing = spacings[0]
+
+        ratios = []
+        
+        for text in page:
+            text["fontSize"] = normalize(text["fontSize"], minFontSize, maxFontSize)
+            text["spacing"] = normalize(text["spacing"], minSpacing, maxSpacing)
+            text["ratio"] = 0
+            if text["spacing"] > 0:
+                text["ratio"] = text["fontSize"]/text["spacing"]
+            ratios.append(text["ratio"])
+
+        # sort ratios and get min and max
+        ratios.sort()
+        minRatio = ratios[0]
+        maxRatio = ratios[len(ratios)-1]
+
+        for text in page:
+            text["ratio"] = normalize(text["ratio"], minRatio, maxRatio)
+
 
 @application.route('/')
 def index():
@@ -14,10 +58,14 @@ def index():
 
 @application.route('/compute-text-stats/', methods = ['GET','POST'])
 def computeTextStats():
+    start = time.time()
     pdf = request.json
-    doc = Document(pdf)
-    #response = {}
-    return jsonify(vars(doc))
-
+    processTexts(pdf)
+    end = time.time()
+    response = {}
+    response["pdf"] = pdf
+    response["time"] = round(end- start, 4)
+    return jsonify(response)
+    
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
--- models.py
+++ models.py
@@ -15,110 +15,55 @@ class Document:
     def __init__(self, pdf):
         # create pages
         self.pages = self.createPages(pdf)
-        # create font size stats
-        self.docWordCount = self.getDocWordCount(self.pages)
-        self.docLineCount = self.getDocLineCount(self.pages)
-        #self.pageCount =
-        docTexts = []
-        for page in self.pages:
-            docTexts += page["pageTexts"]
-        self.docFontSizeStats = vars(FontSizeStats(docTexts, self.docWordCount))
 
     def createPages(self, pdf):
         pages = []
         
         for i, pageItem in enumerate(pdf):
-            #print(json.dumps(pageItem))
             page = vars(Page(pageItem, i))
             pages.append(page)
         
         return pages
-    
-    def getDocWordCount(self, pages):
-        docWordCount = 0
-        for page in pages:
-            docWordCount += page["pageWordCount"]
-        return docWordCount
-    
-    def getDocLineCount(self, pages):
-        docLineCount = 0
-        for page in pages:
-            docLineCount += page["pageLineCount"]
-        return docLineCount
 
 class Page:
 
     def __init__(self, textItems, pageNumber):
         self.pageNumber = pageNumber
         self.pageTexts = self.createPageTexts(textItems)
-        self.pageWordCount = self.getPageWordCount(self.pageTexts)
-        self.pageLineCount = self.getPageLineCount(self.pageTexts)
-        self.pageFontSizeStats = vars(FontSizeStats(self.pageTexts, self.pageWordCount))
 
     def createPageTexts(self, textItems):
-        texts = []
-        
-        for item in textItems:
-                #print(json.dumps(item))
-                text = vars(Text(item["str"], item["size"]))
-                texts.append(text)
-        
-        return texts
-    
-    def getPageWordCount(self, texts):
-        wordCount = 0
-        for text in texts:
-            wordCount += text["wordCount"]
-        return wordCount
-
-    def getPageLineCount(self, pageTexts):
-        return len(pageTexts)
+        texts = TextProcesser.processTexts(textItems)
+        return vars(texts)
 
 class Text:
 
-    def __init__(self, str, fontSize):
+    def __init__(self, str, fontSize, length, x, y, spacing, ratio):
         self.str = str
         self.wordCount = self.getWordCount(str)
         self.fontSize = fontSize
+        self.length = length
+        self.x = x
+        self.y = y
+        self.spacing = spacing
+        self.ratio = ratio
 
     def getWordCount(self, str):
         words = str.split()
         return len(words)
 
-
-class FontSize:
-    def __init__(self, size, count, mode, totalCount):
-        self.size = size
-        self.count = count
-        self.frequency = self.getFrequency(count, totalCount)
-        self.deviationFromMode = self.getDeviationFromMode(mode)
-    
-    def getFrequency(self, count, totalCount):
-        return float(count)/totalCount
-    
-    def getDeviationFromMode(self, mode):
-        return self.size - mode
-
-class FontSizeStats:
-    def __init__(self, texts, totalWordCount):
-        # get all font sizes
-        fontSizes = {}
-        for item in texts:
-            size = item["fontSize"]
-            if size in fontSizes:
-                fontSizes[size] += item["wordCount"]
-            else:
-                fontSizes[size] = item["wordCount"]
+class TextProcesser:
+    @staticmethod
+    def processTexts(textItems):
+        # get all the font sizes, max and min
+        fontSizes = []
+        for item in textItems:
+            fontSizes.append(item["fontSize"])
         
-        self.fontSizes = fontSizes
+        # get all the spacing max and min
+        spacings = []
+        
+        pass
 
-        # get mode, largest, and smallest font size
-        self.modeFontSize = max(fontSizes, key=lambda key: fontSizes[key])
-        self.maxFontSize = max(k for k, v in fontSizes.items())
-        self.minFontSize = min(k for k, v in fontSizes.items())
-
-        for fontSize, count in self.fontSizes.items():
-            self.fontSizes[fontSize] = vars(FontSize(fontSize, count, self.modeFontSize, totalWordCount))
 
 
 
